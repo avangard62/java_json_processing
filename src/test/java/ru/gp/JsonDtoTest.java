f721615088f5f51c0bd1224ea5789fd460a32bb4
gp <levapag@rambler.ru>
2024-03-16 23:41:11 +0300

    second example and test for var keys

--- src/test/java/ru/gp/JsonDtoTest.java
+++ src/test/java/ru/gp/JsonDtoTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import ru.gp.dto.CommonEntity;
 
 import java.net.URL;
+import java.util.Map;
 
 public class JsonDtoTest {
     @SneakyThrows
@@ -20,5 +21,17 @@ public class JsonDtoTest {
         System.out.println("Serialized: " + jsonOut);
         Assert.assertEquals(jsonOut, "{\"id\":18,\"userName\":\"AnoNimus777\",\"specKey3\":456.7,\"extraKey\":\"undefined_val\"}");
         Assert.assertEquals(entity.getSpec().get("extraKey"), "undefined_val");
+    }
+
+    @SneakyThrows
+    @Test
+    void testParseVaryingKeysInJson() {
+        ObjectMapper om = new ObjectMapper();
+        URL resource = getClass().getClassLoader().getResource("example-2.json");
+        CommonEntity entity = om.readValue(resource, CommonEntity.class);
+        System.out.println("toString: " + entity);
+        String jsonOut = om.writeValueAsString(entity);
+        System.out.println("Serialized: " + jsonOut);
+        Assert.assertTrue(entity.getSpec().get("SuperPuperKey17") instanceof Map);
     }
 }
